a399a94b2e9017abc36a15ad07e13be5eaa1557a
Jackie Chen <cheezballz3.14@gmail.com>
2019-02-19 21:14:21 -0500

    Add Lift

--- components/low/lift.py
+++ components/low/lift.py
@@ -1,69 +1,80 @@
 import wpilib
 import ctre
+import navx
 
 class Lift:
+    navx: navx.AHRS
+
     lift_front: ctre.WPI_TalonSRX
     lift_back: ctre.WPI_TalonSRX
-    lift_drive_front: wpilib.Victor
-    lift_drive_back: wpilib.Victor
+
+    lift_drive_left: wpilib.Victor
+    lift_drive_right: wpilib.Victor
+
     lift_top_limit_front: wpilib.DigitalInput
     lift_top_limit_back: wpilib.DigitalInput
     lift_bot_limit_front: wpilib.DigitalInput
     lift_bot_limit_back: wpilib.DigitalInput
+
     lift_prox_front: wpilib.DigitalInput
     lift_prox_back: wpilib.DigitalInput
 
     def __init__(self):
+        self.finishedFront = True
+        self.finishedBack = True
+        self.front_speed = 0
+        self.back_speed = 0
+        self.drive_speed = 0
+
+    def drive(self, speed):
+        self.drive_speed = speed
+
+    def liftUp(self, speed):
+        self.frontUp(speed + (1 - speed) * (self.navx.getPitch() / 20))
+        self.backUp(speed + (1 - speed) * -(self.navx.getPitch() / 20))
+
+    def liftDown(self, speed):
+        self.frontUp(speed + (1 - speed) * -(self.navx.getPitch() / 20))
+        self.backUp(speed + (1 - speed) * (self.navx.getPitch() / 20))
+
+    def backUp(self, speed):
+        self.back_speed = speed
+
+    def backDown(self, speed):
+        self.back_speed = -speed
+
+    def frontUp(self, speed):
+        self.front_speed = speed
+    
+    def frontDown(self, speed):
+        self.front_speed = -speed
+
+    def getProxFront(self):
+        return not self.lift_prox_front.get()
+
+    def getProxBack(self):
+        return not self.lift_prox_back.get()
+
+    def finish(self):
+        return self.finishedFront and self.finishedBack
+
+    def stop(self):
         self.finished = True
         self.front_speed = 0
         self.back_speed = 0
-        self.front_drive_speed = 0
-        self.back_drive_speed = 0
-
-    def driveFront(self, speed):
-        self.front_speed = speed
-
-    def driveBack(self, speed):
-        self.back_speed = speed
-
-    def liftUp(self, speed):
-        self.finished = True
-        self.frontUp(speed)
-        self.backUp(speed)
-
-    def liftDown(self, speed):
-        self.finished = True
-        self.frontDown(speed)
-        self.backDown(speed)
-
-    def backUp(self, speed):
-        if not self.lift_top_limit_back.get():
-            self.back_speed = speed
-            self.finished = False
-
-    def backDown(self, speed):
-        if not self.lift_bot_limit_back.get():
-            self.back_speed = -speed
-            self.finished = False
-
-    def frontUp(self, speed):
-        if not self.lift_top_limit_front.get():
-            self.front_speed = speed
-            self.finished = False
-    
-    def frontDown(self, speed):
-        if not self.lift_bot_limit_front.get():
-            self.front_speed = -speed
-            self.finished = False
-
-    def getProxFront(self):
-        return self.lift_prox_front.get()
-
-    def getProxBack(self):
-        return self.lift_prox_back.get()
+        self.drive_speed = 0
 
     def execute(self):
-        self.lift_front.set(self.front_speed)
-        self.lift_back.set(self.back_speed)
-        self.lift_drive_front.set(self.front_drive_speed)
-        self.lift_drive_back.set(self.back_drive_speed)
+        if not self.finishedFront:
+            self.lift_front.set(self.front_speed)
+        else:
+            self.lift_front.set(0)
+        if not self.finishedBack:
+            self.lift_back.set(self.back_speed)
+        else:
+            self.lift_back.set(0)
+        self.lift_drive_left.set(self.drive_speed)
+        self.lift_drive_right.set(self.drive_speed)
+
+        self.finishedFront = not self.lift_prox_front.get()
+        self.finishedBack = not self.lift_prox_back.get()
--- robot.py
+++ robot.py
@@ -5,12 +5,19 @@ import magicbot
 import wpilib
 import ctre
 import os
+import navx
+
+from components.low.lift import Lift
 
 class Robot(magicbot.MagicRobot):
+
+    lift: Lift
 
     def createObjects(self):
         self.logger = logging.getLogger("Robot")
         
+        self.joystick = wpilib.Joystick(0)
+
         # Timer
         self.globalTimer = wpilib.Timer()
         self.globalTimer.start()
@@ -21,14 +28,49 @@ class Robot(magicbot.MagicRobot):
         with open("../buttons.json" if os.getcwd()[-5:-1] == "test" else sys.path[0] + "/buttons.json") as f:
             self.buttons = json.load(f)
         
-        self.liftLimit = wpilib.DigitalInput(ports["lift"]["limit"])
+        # Drive
+        self.front_left = wpilib.Spark(ports["drive"]["front_left"])
+        self.front_right = wpilib.Spark(ports["drive"]["front_right"])
+        self.back_left = wpilib.Spark(ports["drive"]["back_left"])
+        self.back_right = wpilib.Spark(ports["drive"]["back_right"])
+
+        # Lift
+        self.navx = navx.ahrs.AHRS.create_spi()
+
+        self.lift_front = ctre.WPI_TalonSRX(ports["lift"]["lifter"]["front"])
+        self.lift_back = ctre.WPI_TalonSRX(ports["lift"]["lifter"]["back"])
+
+        self.lift_drive_left = wpilib.Victor(ports["lift"]["drive"]["left"])
+        self.lift_drive_right = wpilib.Victor(ports["lift"]["drive"]["right"])
+
+        self.lift_top_limit_front = wpilib.DigitalInput(ports["lift"]["limit"]["top_front"])
+        self.lift_top_limit_back = wpilib.DigitalInput(ports["lift"]["limit"]["top_back"])
+        self.lift_bot_limit_front = wpilib.DigitalInput(ports["lift"]["limit"]["bot_front"])
+        self.lift_bot_limit_back = wpilib.DigitalInput(ports["lift"]["limit"]["bot_back"])
+
+        self.lift_prox_front = wpilib.DigitalInput(ports["lift"]["prox"]["front"])
+        self.lift_prox_back = wpilib.DigitalInput(ports["lift"]["prox"]["back"])
+
 
     def teleopInit(self):
         print("Starting Teleop")
 
     def teleopPeriodic(self):
-        if self.globalTimer.hasPeriodPassed(0.5):
-            self.logger.info(self.liftLimit.get())
+        # Drive
+        try:
+            self.drive.setSpeedsFromJoystick(self.joystick.getX(), self.joystick.getY(), self.joystick.getTwist())
+        except:
+            self.onException()
+        # Arm
+        try:
+            if self.joystick.getRawButton(self.buttons["joy1"]["liftup"]):
+                self.lift.liftUp()
+            elif self.joystick.getRawButton(self.buttons["joy1"]["liftdown"]):
+                self.lift.liftDown()
+            else:
+                self.lift.stop()
+        except:
+            self.onException()
 
     def testInit(self):
         print("Starting Test")
